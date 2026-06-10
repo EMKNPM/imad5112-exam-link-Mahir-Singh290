c6f3da69dd78182874216e82186ea850aa37ad2a
Mahir-Singh290 <ST10514124@myemeris.edu.za>
2026-06-10 13:11:04 +0200

    Declarations in the detailed view screen
    2 buttons and 1 textview

--- IMAD5112Exam/app/src/main/java/com/example/imad5112exam/Detailed_View_Screen.kt
+++ IMAD5112Exam/app/src/main/java/com/example/imad5112exam/Detailed_View_Screen.kt
@@ -1,12 +1,21 @@
 package com.example.imad5112exam
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Detailed_View_Screen : AppCompatActivity() {
+
+    //declarations
+
+    private lateinit var btnDetails: Button
+    private lateinit var tvDetails: TextView
+    private lateinit var btnBack: Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
