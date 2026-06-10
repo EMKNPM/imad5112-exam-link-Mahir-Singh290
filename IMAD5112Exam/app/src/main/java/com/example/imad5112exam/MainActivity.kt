59f469d67e038c8c6a4a54e3811727c09f080b7e
Mahir-Singh290 <ST10514124@myemeris.edu.za>
2026-06-10 09:49:09 +0200

    declaration for button done

--- IMAD5112Exam/app/src/main/java/com/example/imad5112exam/MainActivity.kt
+++ IMAD5112Exam/app/src/main/java/com/example/imad5112exam/MainActivity.kt
@@ -3,12 +3,18 @@
 package com.example.imad5112exam
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Declaration of the Main screen button
+
+    private lateinit var btnMainScreen: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
