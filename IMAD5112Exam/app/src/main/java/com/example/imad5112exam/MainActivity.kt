c1469945f76e73914460a42f537ad1a85d4d84a2
Mahir-Singh290 <ST10514124@myemeris.edu.za>
2026-06-10 09:51:48 +0200

    typecasting of the button done

--- IMAD5112Exam/app/src/main/java/com/example/imad5112exam/MainActivity.kt
+++ IMAD5112Exam/app/src/main/java/com/example/imad5112exam/MainActivity.kt
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //typecasting of main screen button 
+        btnMainScreen = findViewById(R.id.btnMainScreen)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
