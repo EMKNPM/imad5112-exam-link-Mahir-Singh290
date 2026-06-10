fb96abb21d84d0379287706b490b98554b317291
Mahir-Singh290 <ST10514124@myemeris.edu.za>
2026-06-10 09:58:26 +0200

    Button code done
    set on click listener and intent done to go to the main screen which was created

--- IMAD5112Exam/app/src/main/java/com/example/imad5112exam/MainScreen.kt
+++ IMAD5112Exam/app/src/main/java/com/example/imad5112exam/MainScreen.kt
@@ -1,42 +1,16 @@
-// Student Number ST10514124
-//Full name: Mahir Singh
 package com.example.imad5112exam
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    //Declaration of the Main screen button
-
-    private lateinit var btnMainScreen: Button
-
+class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        //typecasting of main screen button
-        btnMainScreen = findViewById(R.id.btnMainScreen)
-
-        //Main screen button to go to the next screen
-        btnMainScreen.setOnClickListener {
-
-            //an intent used to get to the new screen
-            val intent = Intent(this, MainScreen::class.java)
-            startActivity(intent)
-
-
-
-        }
-
-
-
+        setContentView(R.layout.activity_main_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
